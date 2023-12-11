927d2d5952ce5b7a112381ab3efb584130dab55c
Kamal Abdelmalek <144700532+KamalCode98@users.noreply.github.com>
2023-12-11 20:50:14 +0100

    Function that print the environment variables to the standard output.

---
+++ print_env.c
@@ -0,0 +1,30 @@
+#include "shell.h"
+
+/**
+ * print_env - Print environment variables to the standard output
+ *
+ * Description: This function prints the environment variables to the
+ * standard output, followed by a newline character. It then frees the
+ * memory allocated for the command.
+ *
+ * @cmd: Array of tokens representing the command and its arguments.
+ *
+ * Return: 0 on success
+ */
+
+int print_env(char **cmd)
+{
+	unsigned int i = 0;
+
+	while (environ[i])
+	{
+		write(STDOUT_FILENO, environ[i], _strlen(environ[i]));
+		write(STDOUT_FILENO, "\n", 1);
+		i++;
+	}
+
+	/* Free memory allocated for the command */
+	free_memory(cmd);
+
+	return (0);
+}
